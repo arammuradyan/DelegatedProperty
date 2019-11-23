ecf4b50235df29fcae5688b83a3bd283c11c2056
arammuradyan <aram.muradyan2089@gmail.com>
2019-11-23 22:34:02 +0400

    version2

--- src/Main.kt
+++ src/Main.kt
@@ -6,24 +6,25 @@ fun main() {
     val massage="weak reference get() fun. returns null"
 
     // Example with String value
-    val stringValue = WeakRefKeeper("String in Weak Reference")
-    stringValue.value.get()?.also { println(it) } ?: massage.also { print(it) }
+    val stringValue = WeakRefKeeper("String in weak reference")
+    stringValue.value?.also { println(it) } ?: massage.also { println(it) }
+
 
     // Example with Integer value
     val intValue = WeakRefKeeper(massage.length)
-    intValue.value.get()?.also { println(it) } ?: massage.also { print(it) }
+    intValue.value?.also { println(it) } ?: massage.also { print(it) }
 }
 
-class WeakRefKeeper<T>(value: T) {
-    val value: WeakReference<T> by WeakDelegation<WeakRefKeeper<T>, WeakReference<T>>(WeakReference(value))
+class WeakRefKeeper<T>(value: T?) {
+    val value: T? by WeakDelegation<WeakRefKeeper<T>,T>(WeakReference(value))
 }
 
 class WeakDelegation<R, T>(
-    private val value: T
-) : ReadOnlyProperty<R, T> {
+    private val value: WeakReference<T?>
+)  {
 
-    override fun getValue(thisRef: R, property: KProperty<*>): T {
-        return value
+    operator fun getValue(thisRef: R, property: KProperty<*>): T? {
+        return value.get()
     }
 
 }
